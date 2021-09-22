25da172582a39f9b3db69915f403ef98b8cc52cf
abhinavDhulipala <abhinav.dhulipala@berkeley.edu>
2021-09-21 23:38:45 -0700

    go back to base gatsby app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
 module.exports = {
-  siteMetadata: {
-    title: `campy trails`,
-    description: `Stripe storefront starter for Gatsby`,
-    author: `Brock McElroy <brxck.dev>`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    // tsx-plugin options: https://babeljs.io/docs/en/babel-preset-typescript#options
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-typescript`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `campy trails`,
-        short_name: `campy trails`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Price"],
-        secretKey: process.env.STRIPE_SECRET_KEY,
-        downloadFiles: true,
-        auth: false,
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
       },
     },
   ],
-}
+};
