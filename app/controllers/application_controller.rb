4553a7f2f7fafbe57b43515c10af07fb91d4836b
Abhinav Dhulipala <abhinav.dhulipala@berkeley.edu>
2021-10-28 01:44:36 -0700

    registration devise modificaiton complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name occupation])
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,13 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  @@viable_occupations = %i[porter guide]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_inclusion_of :occupation, in: @@viable_occupations, on: :create, message: "not part of viable occupation"
+
+  def self.viable_occupations
+    @@viable_occupations
+  end
 end
