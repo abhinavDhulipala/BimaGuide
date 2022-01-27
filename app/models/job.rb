8f64df980300b87165d2bfb87cd10e009975152c
Abhinav Dhulipala <abhinav.dhulipala@berkeley.edu>
2022-01-27 15:15:55 -0800

    job validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,18 @@ class Job < ApplicationRecord
   belongs_to :employee
   validates_presence_of :duration, :total_pay, :date_completed, :role
   validates_inclusion_of :role, in: Employee.occupations.keys
-  validate :date_completed, if: ->{errors.add(:date_completed, 'must enter a date within a month from now') if date_completed and date_completed < 1.month.ago}
+  validate :date_completed, if: ->{errors.add(:date_completed, 'must enter a date within a month from now') if date_completed.present? and date_completed < 1.month.ago}
+  validate :no_overlap_dates
   has_one_attached :document
+
+  private
+
+  def no_overlap_dates
+    # if any jobs previous end date is greater than our current start date
+    # that doesn't make logical sense
+    start_date = date_completed - duration.days
+    if Job.where(employee_id: employee_id).any? { |j| j.date_completed > start_date }
+      errors.add(:date_completed, "Invalid dates please check: you couldn't have started a job during another active job")
+    end
+  end
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do 
+    @employee = employees(:default)
+  end
+
+  test 'job validation: job cannot be started while on prior job' do 
+      job1 = @employee.jobs.create!(date_completed: 10.days.ago, duration: 5, total_pay: 80, role: 'guide')
+
+      # job starts todays before the end of job 1, should be invalid
+      job2 = @employee.jobs.create(date_completed: 5.days.ago, duration: 7, total_pay: 80, role: 'guide')
+      refute_predicate job2, :persisted?
+      assert_includes job2.errors, :date_completed
+      assert_equal job2.errors[:date_completed], ["Invalid dates please check: you couldn't have started a job during another active job"]
+      
+      job2 = @employee.jobs.create(date_completed: 5.days.ago, duration: 5, total_pay: 80, role: 'guide')
+      assert_predicate job2, :persisted?
+  end
 end
